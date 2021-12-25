670b5b91f7a37517b98ff5170454b4b9e1f00e0e
BaticSea <81511540+BaticSea@users.noreply.github.com>
2021-12-25 17:11:40 +0000

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'FanDao'
+copyright = '2021, Hank Zimmer'
+author = 'Hank Zimmer'
 
 release = '0.1'
 version = '0.1.0'
